eeaaa95f7a042a9a54d9581f7dd896bae37b0aef
ted <tuncay2020@gmail.com>
2020-02-07 18:25:30 -0500

    wordcloud, lemmatizer, topic modeling, tfidf
    wordcloud, lemmatizer, topic modeling, tfidf

--- fake news detection with python.py
+++ fake news detection with python.py
@@ -142,3 +142,194 @@ def remove_punctuation(s):
 
 df['title2_stemmed2']=df['title2_stemmed'].apply(remove_punctuation)
 df['text2_stemmed2']=df['text2_stemmed'].apply(remove_punctuation)
+
+
+
+
+lemztr = WordNetLemmatizer()
+# df['text2_stemmed2_lem'] = df['text2_stemmed2'].apply(lambda x: [lemztr.lemmatize(y) for y in x])
+# df['title2_stemmed2_lem'] = df['title2_stemmed2'].apply(lambda x: [lemztr.lemmatize(y) for y in x])
+
+
+# In[18]:
+
+
+df2=df[["label","text2_stemmed2","title2_stemmed2"]]
+
+
+# In[24]:
+
+
+df2.head()
+
+
+# In[50]:
+
+
+# ngrams
+# TextBlob(df2['text2_stemmed2'][5]).ngrams(3)
+
+
+# In[55]:
+
+
+wc = WordCloud(background_color="white", max_words=2000, width=800, height=400)
+# generate word cloud
+wc.generate(' '.join(df2['text2_stemmed2']))
+
+# show
+plt.figure(figsize=(12, 6))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+# further cleaning is required for unwanted terms
+
+
+# In[ ]:
+
+
+# sentiment analysis
+# coming up...
+
+
+# In[ ]:
+
+
+
+
+
+# In[58]:
+
+
+# define labels
+labels=df2.label
+labels.head()
+
+
+# In[59]:
+
+
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+
+# In[60]:
+
+
+# tfidf vectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+
+# In[61]:
+
+
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+
+# In[62]:
+
+
+confusion_matrix(y_test,y_pred, labels=['fake','real'])
+
+
+# In[63]:
+
+
+# naive bayes classifier
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# topic modeling
+
+
+# In[76]:
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+count_vect = CountVectorizer(max_df=0.8, min_df=2, stop_words='english')
+doc_term_matrix = count_vect.fit_transform(df2['text2_stemmed2'].values.astype('U'))
+
+
+# In[77]:
+
+
+from sklearn.decomposition import LatentDirichletAllocation
+
+LDA = LatentDirichletAllocation(n_components=5, random_state=42)
+LDA.fit(doc_term_matrix)
+
+
+# In[78]:
+
+
+import random
+
+for i in range(10):
+    random_id = random.randint(0,len(count_vect.get_feature_names()))
+    print(count_vect.get_feature_names()[random_id])
+
+
+# In[79]:
+
+
+first_topic = LDA.components_[0]
+top_topic_words = first_topic.argsort()[-10:]
+for i in top_topic_words:
+    print(count_vect.get_feature_names()[i])
+
+
+# In[80]:
+
+
+# 10 words with highest probabilities for five topics
+for i,topic in enumerate(LDA.components_):
+    print(f'Top 10 words for topic #{i}:')
+    print([count_vect.get_feature_names()[i] for i in topic.argsort()[-10:]])
+    print('\n')
+
+
+# In[84]:
+
+
+topic_values = LDA.transform(doc_term_matrix)
+topic_values.shape
+
+
+# In[83]:
+
+
+# add a topic column
+df2['topic'] = topic_values.argmax(axis=1)
+df2.head()
+
+
+# In[ ]:
